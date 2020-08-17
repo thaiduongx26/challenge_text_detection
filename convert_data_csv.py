5485b90aaae20f4afabc1bd19ac78326210ada11
howard <thaiduongx26@gmail.com>
2020-08-17 16:16:47 +0700

    update fix bug csv file

--- convert_data_csv.py
+++ convert_data_csv.py
@@ -4,4 +4,5 @@ import pandas as pd
 if __name__ == "__main__":
     convert_data_to_csv('data/images/', 'data/labels/')
     class_list_df = pd.DataFrame({'class_name': ['vertical'], 'id': [0]})
-    class_list_df.to_csv('class_list.csv')
+    class_list_df.to_csv('class_list.csv', index=False, header=False)
+
