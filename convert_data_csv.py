56d73ef0af38ddd488d38486e5fb5d99ba19cc04
howard <thaiduongx26@gmail.com>
2020-08-17 15:58:54 +0700

    update code gen class list

--- convert_data_csv.py
+++ convert_data_csv.py
@@ -1,4 +1,7 @@
 from process.data_generation import convert_data_to_csv
+import pandas as pd
 
 if __name__ == "__main__":
     convert_data_to_csv('data/images/', 'data/labels/')
+    class_list_df = pd.DataFrame({'class_name': 'vertical', 'id': 0})
+    class_list_df.to_csv('class_list.csv')
