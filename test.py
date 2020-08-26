52bda40e4b2051ae5cae96b9d01247290f6ba6a4
howard <thaiduongx26@gmail.com>
2020-08-26 12:03:21 +0700

    update

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 from process import draw
+import cv2
+import os
 if __name__ == "__main__":
-    draw.draw_rectangle_image('3023.png', '3023.json')
+    min_w = 0
+    min_h = 0
+    list_imgs = os.listdir('data/images')
+    for img_name in list_imgs:
+        img = cv2.imread(os.path.join('data/images', img_name))
+        if img.shape[0] < min_h:
+            min_h = img.shape[0]
+        if img.shape[1] < min_w:
+            min_w = img.shape[1]
+
+    print('min_h: ', min_h)
+    print('min_w: ', min_w)
