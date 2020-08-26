5c3429ad9cc361f012401270b79218c3f5cc9ed8
howard <thaiduongx26@gmail.com>
2020-08-26 12:35:34 +0700

    update

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import cv2
 import os
 from tqdm import tqdm
 if __name__ == "__main__":
-    min_w = 0
-    min_h = 0
+    min_w = 1000000000
+    min_h = 1000000000
     list_imgs = os.listdir('data/images')
     for img_name in tqdm(list_imgs):
         img = cv2.imread(os.path.join('data/images', img_name))
