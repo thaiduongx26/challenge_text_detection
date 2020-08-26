ef69a610e2e542c8f9f75631670477b00ce2b1b4
howard <thaiduongx26@gmail.com>
2020-08-26 12:09:52 +0700

    update

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from process import draw
 import cv2
 import os
+from tqdm import tqdm
 if __name__ == "__main__":
     min_w = 0
     min_h = 0
     list_imgs = os.listdir('data/images')
-    for img_name in list_imgs:
+    for img_name in tqdm(list_imgs):
         img = cv2.imread(os.path.join('data/images', img_name))
         if img.shape[0] < min_h:
             min_h = img.shape[0]
